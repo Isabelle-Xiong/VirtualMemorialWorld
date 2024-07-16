2d388faf5f9b3aaa56a250ab0b5944e3020ba47a
isabelle-xiong <yeelamisabelle.xiong@duke.edu>
2024-07-16 00:22:00 -0400

    updated navigation logic

--- frontend2/src/components/Navigation.js
+++ frontend2/src/components/Navigation.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../Navigation.css'; 
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import '../Navigation.css';
 
 function Navigation() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        // Check login status from localStorage
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!token);
+    }, []);
+
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        setIsLoggedIn(false);
+        navigate('/');
+    };
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
@@ -11,25 +26,34 @@ function Navigation() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav me-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/register">Register</Link>
-                        </li>
-                        <li className="nav-item">
                             <Link className="nav-link" to="/avatar">Create Avatar</Link>
                         </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/profile">Profile</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/logout">Logout</Link>
-                        </li>
+                        {isLoggedIn && (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/profile">Profile</Link>
+                            </li>
+                        )}
+                    </ul>
+                    <ul className="navbar-nav ms-auto">
+                        {!isLoggedIn ? (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/login">Login</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/register">Register</Link>
+                                </li>
+                            </>
+                        ) : (
+                            <li className="nav-item">
+                                <button className="nav-link btn btn-link" onClick={handleLogout}>Logout</button>
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
