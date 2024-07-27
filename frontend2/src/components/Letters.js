00d2e99b5dca6ea9724afbdb6c6fbc5f99bdf26d
isabelle-xiong <yeelamisabelle.xiong@duke.edu>
2024-07-27 01:27:53 -0400

    fixed css

--- frontend2/src/components/Letters.js
+++ frontend2/src/components/Letters.js
@@ -68,27 +68,31 @@ function Letters() {
 
     const toggleReceivedLetters = () => {
         setShowReceivedLetters(!showReceivedLetters);
+        const historyElement = document.querySelector('.letters-page-letter-history');
+        if (historyElement) {
+            historyElement.classList.toggle('letters-page-visible');
+        }
     };
 
     return (
         <div className="letters-page-background">
-            <div className="letters-container">
-                <div className="header-container">
+            <div className="letters-page-container">
+                <div className="letters-page-header-container">
                     <h2>
                         {!selectedLetter ? `Letters to ${avatarName}` : `Letter Title: ${selectedLetter.title}`}
                     </h2>
-                    <div className="mailbox-toggle" onClick={toggleReceivedLetters}>
+                    <div className="letters-page-mailbox-toggle" onClick={toggleReceivedLetters}>
                         {showReceivedLetters ? (
                             <img
                                 src="https://cdn.iconscout.com/icon/free/png-256/free-mail-mailbox-open-postbox-38048.png"
                                 alt="Open Mailbox"
-                                className="mailbox-icon"
+                                className="letters-page-mailbox-icon"
                             />
                         ) : (
                             <img
                                 src="https://www.svgrepo.com/show/404987/closed-mailbox-with-raised-flag.svg"
                                 alt="Closed Mailbox"
-                                className="mailbox-icon"
+                                className="letters-page-mailbox-icon"
                             />
                         )}
                     </div>
@@ -96,7 +100,7 @@ function Letters() {
                 {!selectedLetter ? (
                     <>
                         <div
-                            className="letter-writing-area"
+                            className="letters-page-letter-writing-area"
                             style={{ backgroundImage: `url(${selectedBackground})` }}
                         >
                             <input
@@ -111,20 +115,20 @@ function Letters() {
                                 onChange={(e) => setLetterContent(e.target.value)}
                             />
                         </div>
-                        <div className="background-selection">
+                        <div className="letters-page-background-selection">
                             <label>Select Background:</label>
-                            <div className="background-options">
+                            <div className="letters-page-background-options">
                                 <div
-                                    className={`background-option ${selectedBackground === 'https://img.freepik.com/free-vector/watercolor-abstract-background_23-2148995294.jpg' ? 'selected' : ''}`}
+                                    className={`letters-page-background-option ${selectedBackground === 'https://img.freepik.com/free-vector/watercolor-abstract-background_23-2148995294.jpg' ? 'letters-page-selected' : ''}`}
                                     onClick={() => setSelectedBackground('https://img.freepik.com/free-vector/watercolor-abstract-background_23-2148995294.jpg')}
                                 >
                                     <img
-                                        src="https://img.freepik.com/free-photo/wall-blank-paper-frame-with-acorn-decoration_53876-105706.jpghttps://img.freepik.com/free-vector/watercolor-abstract-background_23-2148995294.jpg"
+                                        src="https://img.freepik.com/free-vector/watercolor-abstract-background_23-2148995294.jpg"
                                         alt="Background 1"
                                     />
                                 </div>
                                 <div
-                                    className={`background-option ${selectedBackground === 'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm00MjItMDQ3LXguanBn.jpg' ? 'selected' : ''}`}
+                                    className={`letters-page-background-option ${selectedBackground === 'https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm00MjItMDQ3LXguanBn.jpg' ? 'letters-page-selected' : ''}`}
                                     onClick={() => setSelectedBackground('https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm00MjItMDQ3LXguanBn.jpg')}
                                 >
                                     <img
@@ -133,7 +137,7 @@ function Letters() {
                                     />
                                 </div>
                                 <div
-                                    className={`background-option ${selectedBackground === 'https://slidescorner.com/wp-content/uploads/2024/01/Powerpoint-Background-Watercolor-Birthday-Party-or-Celebration-Balloons-by-SlidesCorner.com_.jpg' ? 'selected' : ''}`}
+                                    className={`letters-page-background-option ${selectedBackground === 'https://slidescorner.com/wp-content/uploads/2024/01/Powerpoint-Background-Watercolor-Birthday-Party-or-Celebration-Balloons-by-SlidesCorner.com_.jpg' ? 'letters-page-selected' : ''}`}
                                     onClick={() => setSelectedBackground('https://slidescorner.com/wp-content/uploads/2024/01/Powerpoint-Background-Watercolor-Birthday-Party-or-Celebration-Balloons-by-SlidesCorner.com_.jpg')}
                                 >
                                     <img
@@ -142,7 +146,7 @@ function Letters() {
                                     />
                                 </div>
                                 <div
-                                    className={`background-option ${selectedBackground === 'https://c1.wallpaperflare.com/preview/258/359/614/596edafe6db62.jpg' ? 'selected' : ''}`}
+                                    className={`letters-page-background-option ${selectedBackground === 'https://c1.wallpaperflare.com/preview/258/359/614/596edafe6db62.jpg' ? 'letters-page-selected' : ''}`}
                                     onClick={() => setSelectedBackground('https://c1.wallpaperflare.com/preview/258/359/614/596edafe6db62.jpg')}
                                 >
                                     <img
@@ -151,7 +155,7 @@ function Letters() {
                                     />
                                 </div>
                                 <div
-                                    className={`background-option ${selectedBackground === 'https://img.freepik.com/free-vector/watercolor-background-world-tourism-day_23-2149567156.jpg' ? 'selected' : ''}`}
+                                    className={`letters-page-background-option ${selectedBackground === 'https://img.freepik.com/free-vector/watercolor-background-world-tourism-day_23-2149567156.jpg' ? 'letters-page-selected' : ''}`}
                                     onClick={() => setSelectedBackground('https://img.freepik.com/free-vector/watercolor-background-world-tourism-day_23-2149567156.jpg')}
                                 >
                                     <img
@@ -160,7 +164,7 @@ function Letters() {
                                     />
                                 </div>
                                 <div
-                                    className={`background-option ${selectedBackground === 'https://static.vecteezy.com/system/resources/previews/042/344/243/non_2x/watercolor-pastel-color-background-design-vector.jpg' ? 'selected' : ''}`}
+                                    className={`letters-page-background-option ${selectedBackground === 'https://static.vecteezy.com/system/resources/previews/042/344/243/non_2x/watercolor-pastel-color-background-design-vector.jpg' ? 'letters-page-selected' : ''}`}
                                     onClick={() => setSelectedBackground('https://static.vecteezy.com/system/resources/previews/042/344/243/non_2x/watercolor-pastel-color-background-design-vector.jpg')}
                                 >
                                     <img
@@ -170,22 +174,22 @@ function Letters() {
                                 </div>
                             </div>
                         </div>
-                        <button onClick={sendLetter}>Send</button>
+                        <button className="letters-page-send-button" onClick={sendLetter}>Send</button>
                     </>
                 ) : null}
-                <div className="response-area">
+                <div className="letters-page-response-area">
                     {selectedLetter ? (
-                        <div className="letter-details" style={{ backgroundImage: `url(${selectedLetter.background})` }}>
+                        <div className="letters-page-letter-details" style={{ backgroundImage: `url(${selectedLetter.background})` }}>
                             <h4>{selectedLetter.title}</h4>
                             <p><strong>You:</strong> {selectedLetter.content}</p>
                             <p><strong>Response:</strong> {selectedLetter.response}</p>
-                            <button onClick={() => setSelectedLetter(null)}>Back to List</button>
+                            <button className="letters-page-back-button" onClick={() => setSelectedLetter(null)}>Back to List</button>
                         </div>
                     ) : (
-                        <div className={`letter-history ${showReceivedLetters ? 'visible' : 'hidden'}`}>
+                        <div className={`letters-page-letter-history ${showReceivedLetters ? 'letters-page-visible' : 'letters-page-hidden'}`}>
                             <h3>Letter History</h3>
                             {letters.map((letter, index) => (
-                                <div key={index} className="letter-item" onClick={() => handleLetterClick(letter)}>
+                                <div key={index} className="letters-page-letter-item" onClick={() => handleLetterClick(letter)}>
                                     <p>Letter Title: {letter.title}</p>
                                 </div>
                             ))}
